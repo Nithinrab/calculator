d914c6143667f99335e6c296e302bf8d48434b63
Nithinrab <nithinrab08@gmail.com>
2022-03-15 15:54:35 +0530

    edit

--- src/com/harman/batch4/calc.java
+++ src/com/harman/batch4/calc.java
@@ -4,14 +4,20 @@ package com.harman.batch4;
         import java.util.Scanner;
 
 public class calc {
+    public int AdditionOperation(int x,int y)
+    {
+        int z=x+y;
+        return z;
+    }
     public static void main(String[] args) {
+        calc cal = new calc();
         Scanner input=new Scanner(System.in);
         int n1,n2,result;
         System.out.println("enter num1");
         n1=input.nextInt();
         System.out.println("enter num2");
         n2=input.nextInt();
-        result=n1+n2;
+        result=cal.AdditionOperation(n1,n2);
         System.out.println(result);
     }
 }
