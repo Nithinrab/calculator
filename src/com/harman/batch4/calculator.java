a8668e5bf4863cf358d995b1567b946e3ee60574
Nithinrab <nithinrab08@gmail.com>
2022-03-16 12:02:17 +0530

    addition operation with function

--- src/com/harman/batch4/calculator.java
+++ src/com/harman/batch4/calculator.java
@@ -5,14 +5,19 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class calculator {
+    public int AdditionOperation(int x,int y){
+        int z=x+y;
+        return z;
+    }
     public static void main(String[] args) {
+        calculator calc=new calculator();
         Scanner input=new Scanner(System.in);
         int n1,n2, result;
         System.out.println("enter num1");
         n1 = input.nextInt();
         System.out.println("enter num2");
         n2 = input.nextInt();
-        result=n1+n2;
+        result=calc.AdditionOperation(n1,n2);
         System.out.println(result);
     }
 }
