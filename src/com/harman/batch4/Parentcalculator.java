634b9ba22bfd7771afede6818469419488d32065
Nithinrab <nithinrab08@gmail.com>
2022-03-16 12:48:36 +0530

    parent calculator

---
+++ src/com/harman/batch4/Parentcalculator.java
@@ -0,0 +1,14 @@
+package com.harman.batch4;
+
+public class Parentcalculator {
+    public int AddOperation(int x,int y){
+        return x + y;
+    }
+    public int SubOperation(int x,int y){
+        return x - y;
+    }
+    public int MulOperation(int x,int y){
+        return x * y;
+    }
+
+}
--- src/com/harman/batch4/calculator.java
+++ src/com/harman/batch4/calculator.java
@@ -4,26 +4,22 @@ package com.harman.batch4;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class calculator {
+public class calculator extends Parentcalculator {
 
     public static void main(String[] args) {
-        Addition adCalc=new Addition();
-        Subtraction subCalc = new Subtraction();
-        Multiplication mulCalc = new Multiplication();
-        Division divCalc = new Division();
+     calculator ob=new calculator();
+
         Scanner input=new Scanner(System.in);
-        int n1,n2, result1,result2,result3,result4;
+        int n1,n2, result1,result2,result3;
         System.out.println("enter num1");
         n1 = input.nextInt();
         System.out.println("enter num2");
         n2 = input.nextInt();
-        result1=adCalc.AdditionOperation(n1,n2);
-        result2=subCalc.SubtractionOperation(n1,n2);
-        result3=mulCalc.MultiplicationOperation(n1,n2);
-        result4=divCalc.DivisionOperation(n1,n2);
+        result1=ob.AddOperation(n1,n2);
+        result2=ob.SubOperation(n1,n2);
+        result3=ob.MulOperation(n1,n2);
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
-        System.out.println(result4);
     }
 }
