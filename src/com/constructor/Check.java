c1ce90dc8b47da55d62daca0065809dd24d5d0bf
Nithinrab <nithinrab08@gmail.com>
2022-03-16 15:27:08 +0530

    exception Check1

--- src/com/constructor/Check.java
+++ src/com/constructor/Check.java
@@ -3,13 +3,16 @@ package com.constructor;
 public class Check {
     public static void main(String[] args) {
         try{
-            int x=10,y=0,z;
-            z=x/y;
-            System.out.println(z);
+            String name = null;
+            System.out.println(name.length());
         }
-        catch(Exception e)
+        catch(ArithmeticException e)
         {
-            System.out.println(e);
+            System.out.println("Divide by Zero is not possible");
+        }
+        catch(NullPointerException e)
+                {
+            System.out.println("NULL VALUE is not accepted");
         }
     }
 }
